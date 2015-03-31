eeb3d0c8f8104c8594f52f80b08f3f6fb57628a0
Franck Audiger <audiger2u@fst-ats-311-l06.fst.site.univ-lorraine.fr>
2015-03-31 19:53:21 +0200

    modification du graph pour pouvoir avoir plus que 8 arrete sur les deux noeud ajouter en source et en destination

--- modelisation/Graph.java
+++ modelisation/Graph.java
@@ -3,66 +3,62 @@ package modelisation;
 import java.util.ArrayList;
 import java.io.*;
 
-public class Graph
-{
-   private ArrayList<Edge>[] adj;
-   private final int V;
-   int E;
-@SuppressWarnings("unchecked")
-   public Graph(int N)
-	 {
+public class Graph {
+	private ArrayList<Edge>[] adj;
+	private final int V;
+	int E;
+
+	@SuppressWarnings("unchecked")
+	public Graph(int N) {
 		this.V = N;
 		this.E = 0;
-		 adj = (ArrayList<Edge>[]) new ArrayList[N];
-		for (int v= 0; v < N; v++)
-		  adj[v] = new ArrayList<Edge>(8);
-		
-	 }
+		adj = (ArrayList<Edge>[]) new ArrayList[N];
+		// modification du dernier noeud pour accepter plus d'arrete que 8
+		adj[0] = new ArrayList<Edge>();
+		for (int v = 1; v < N - 1; v++) {
 
-   public int vertices()
-	 {
+			adj[v] = new ArrayList<Edge>(8);
+		}
+		// modification du dernier noeud pour accepter plus d'arrete que 8
+		adj[N - 1] = new ArrayList<Edge>();
+	}
+
+	public int vertices() {
 		return V;
-	 }
-   
-   public void addEdge(Edge e)
-	 {
+	}
+
+	public void addEdge(Edge e) {
 		int v = e.from;
 		int w = e.to;
 		adj[v].add(e);
 		adj[w].add(e);
-	 }
-   
-   public final Iterable<Edge> adj(int v)
-	 {
+	}
+
+	public final Iterable<Edge> adj(int v) {
 		return adj[v];
-	 }      
-   
-   public final Iterable<Edge> edges()
-	 {
+	}
+
+	public final Iterable<Edge> edges() {
 		ArrayList<Edge> list = new ArrayList<Edge>();
-        for (int v = 0; v < V; v++)
-            for (Edge e : adj(v)) {
-                if (e.to != v)
-                    list.add(e);
-            }
-        return list;
-    }
-   
-   
-   public void writeFile(String s)
-	 {
-		try
-		  {			 
-			 PrintWriter writer = new PrintWriter(s, "UTF-8");
-			 writer.println("digraph G{");
-			 for (Edge e: edges())
-			   writer.println(e.from + "->" + e.to + "[label=\"" + e.used  +"/" + e.capacity + "\"];");
-			 writer.println("}");
-			 writer.close();
-		  }
-		catch (IOException e)
-		  {
-		  }						
-	 }
-   
+		for (int v = 0; v < V; v++)
+			for (Edge e : adj(v)) {
+				if (e.to != v)
+					list.add(e);
+			}
+		return list;
+	}
+
+	public void writeFile(String s) {
+		try {
+			PrintWriter writer = new PrintWriter(s, "UTF-8");
+			writer.println("digraph G{");
+			for (Edge e : edges())
+				writer.println(e.from + "->" + e.to + "[label=\"" + e.used
+						+ "/" + e.capacity + "\"];");
+			writer.println("}");
+			writer.close();
+		} catch (IOException e) {
+		}
+	}
+
 }
