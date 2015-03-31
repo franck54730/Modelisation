bd0c09edbf1a10e2c8aa4c0e6ba2772525a04003
Franck Audiger <audiger2u@fst-ats-311-l06.fst.site.univ-lorraine.fr>
2015-03-31 18:08:46 +0200

    ajout de la fonction interrest de steamcarving

--- modelisation/SeamCarving.java
+++ modelisation/SeamCarving.java
@@ -1,42 +1,80 @@
 package modelisation;
 
-import java.util.ArrayList;
-import java.io.*;
-import java.util.*;
-public class SeamCarving
-{
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-   public static int[][] readpgm(String fn)
-	 {		
-        try {
-            InputStream f = ClassLoader.getSystemClassLoader().getResourceAsStream(fn);
-            BufferedReader d = new BufferedReader(new InputStreamReader(f));
-            String magic = d.readLine();
-            String line = d.readLine();
-		   while (line.startsWith("#")) {
-			  line = d.readLine();
-		   }
-		   Scanner s = new Scanner(line);
-		   int width = s.nextInt();
-		   int height = s.nextInt();		   
-		   line = d.readLine();
-		   s = new Scanner(line);
-		   int maxVal = s.nextInt();
-		   int[][] im = new int[height][width];
-		   s = new Scanner(d);
-		   int count = 0;
-		   while (count < height*width) {
-			  im[count / width][count % width] = s.nextInt();
-			  count++;
-		   }
-		   return im;
-        }
-		
-        catch(Throwable t) {
-            t.printStackTrace(System.err) ;
-            return null;
-        }
-    }
+public class SeamCarving {
 
-   
+	//TODO a degager pour le rendu sert juste a faire des tests
+	public SeamCarving(){
+		int[][] test = {
+				{3,11,24,39},
+				{8,21,29,39},
+				{74,80,100,200}
+		};
+		int[][] interrest = interest(test);
+	}
+	
+	public static int[][] readpgm(String fn) {
+		try {
+			InputStream f = ClassLoader.getSystemClassLoader()
+					.getResourceAsStream(fn);
+			BufferedReader d = new BufferedReader(new InputStreamReader(f));
+			String magic = d.readLine();
+			String line = d.readLine();
+			while (line.startsWith("#")) {
+				line = d.readLine();
+			}
+			Scanner s = new Scanner(line);
+			int width = s.nextInt();
+			int height = s.nextInt();
+			line = d.readLine();
+			s = new Scanner(line);
+			int maxVal = s.nextInt();
+			int[][] im = new int[height][width];
+			s = new Scanner(d);
+			int count = 0;
+			while (count < height * width) {
+				im[count / width][count % width] = s.nextInt();
+				count++;
+			}
+			return im;
+		}
+
+		catch (Throwable t) {
+			t.printStackTrace(System.err);
+			return null;
+		}
+	}
+
+	public static int[][] interest(int[][] image) {
+		int hauteur = image.length;
+		int[][] rep = null;
+		if (hauteur > 0) {
+			int largeur = image[0].length;
+			rep = new int[hauteur][largeur];
+			for (int x = 0; x < largeur; x++) {
+				for (int y = 0; y < hauteur; y++) {
+					int valeurThis = image[y][x];
+					int valeurAvg = 0;
+					if(x == 0){//pixel de gauche
+						valeurAvg = image[y][x+1];
+					}else if(x == largeur-1){//pixel de droite
+						valeurAvg = image[y][x-1];
+					}else{//autre pixel
+						valeurAvg = (image[y][x+1]+image[y][x-1])/2;
+					}
+					rep[y][x] =  Math.abs(Math.abs(valeurThis)-Math.abs(valeurAvg));
+				}
+			}
+		}
+		return rep;
+	}
+
+	//TODO a degager pour le rendu sert juste a faire des tests
+	public static void main(String[] args) {
+		new SeamCarving();
+	}
 }
